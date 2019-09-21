c39c0cb24b72dc228d6aaa3daea6c04150283cd9
vacca_j <jordan.vacca@epitech.eu>
2019-09-21 14:31:01 +0200

    [+] Creation of the presentationView

---
+++ Hollywood/Controller/PresentationVC.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Hollywood
+//
+//  Created by Jordan on 21/09/2019.
+//  Copyright © 2019 Jordan. All rights reserved.
+//
+
+import UIKit
+
+class PresentationVC: UIViewController {
+    
+    @IBOutlet weak var presentationImg: UIImageView!
+    @IBOutlet weak var presentationBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    func setupUI() {
+        
+        //Pic
+        presentationImg.layer.cornerRadius = self.presentationImg.frame.size.width / 2
+        presentationImg.clipsToBounds = true
+        presentationImg.layer.borderWidth = 3
+        presentationImg.layer.borderColor = #colorLiteral(red: 0.2078431373, green: 0.1882352941, blue: 0.3294117647, alpha: 1)
+        presentationImg.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        presentationImg.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        presentationImg.layer.shadowOpacity = 1.0
+        presentationImg.layer.shadowRadius = 0.0
+        
+        //Btn
+        
+        presentationBtn.layer.cornerRadius = 5
+        presentationBtn.layer.borderWidth = 1
+        presentationBtn.layer.borderColor = UIColor.white.cgColor
+        
+    }
+}
+
