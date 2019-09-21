9b48c458fa4c3e1d31c48f7054072bbe4c5a7e58
vacca_j <jordan.vacca@epitech.eu>
2019-09-21 13:59:52 +0200

    Initial Commit

---
+++ Hollywood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hollywood
+//
+//  Created by Jordan on 21/09/2019.
+//  Copyright © 2019 Jordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
